786ecd20098bed61127bc8db7c1ce350ecc1adef
magyardanielmark <magyardanielmark@gmail.com>
2023-10-26 18:19:25 +0200

    finish addCat

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
-import { bubble as Menu } from "react-burger-menu";
-import "./Navbar.css";
+import { bubble as Menu } from 'react-burger-menu';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { useUserContext } from '../../contexts/userContext';
 
 export default function Navbar() {
-  return (
-      <div id="menu-wrapper">
-        <Menu outerContainerId={"menu-wrapper"}>
-          <a id="profile" className="menu-item" href="/">
-            Profile
-          </a>
-          <a id="mycats" className="menu-item" href="/mycats">
-            MY cats
-          </a>
-          <a id="Browsebreeds" className="menu-item" href="/contact">
-            Browse breeds
-          </a>
-        </Menu>
-      </div>
-  );
+    const { currUser, setCurrUser } = useUserContext();
+    return (
+        <div id='menu-wrapper'>
+            <Menu outerContainerId={'menu-wrapper'}>
+                <Link to={`/profile/${currUser._id}`} className='menu-item'>
+                    Profile
+                </Link>
+                <Link to={`/mycats`} className='menu-item'>
+                    MY cats
+                </Link>
+                <Link to={`/breeds`} className='menu-item'>
+                    Browse breeds
+                </Link>
+            </Menu>
+        </div>
+    );
 }
