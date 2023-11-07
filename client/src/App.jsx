257bf2ce0950b64ddd943feef3cf6390d39a20ea
magyardanielmark <magyardanielmark@gmail.com>
2023-11-07 09:22:06 +0100

    breeds features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,36 @@ import MyCats from './pages/MyCats/MyCats';
 import AppContext from './contexts/userContext';
 import CatProfile from './pages/CatProfile/CatProfile';
 import HealthRecord from './pages/HealthRecord/HealthRecord';
+import Breeds from './pages/Breeds/Breeds';
+import Breed from './pages/Breed/Breed';
 
 function App() {
     return (
-      <BrowserRouter>
-        <AppContext>
-          <Routes>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/mycats" element={<MyCats />}></Route>
-            <Route path="/mycats/:catID" element={<CatProfile />}></Route>
-            <Route path='/newhealthrecord' element={<HealthRecord />}></Route>
-          </Routes>
-        </AppContext>
-      </BrowserRouter>
+        <BrowserRouter>
+            <AppContext>
+                <Routes>
+                    <Route path='/' index element={<Home />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/signup' element={<Signup />}></Route>
+                    <Route path='/mycats' element={<MyCats />}></Route>
+                    <Route
+                        path='/mycats/:catID'
+                        element={<CatProfile />}
+                    ></Route>
+                    <Route
+                        path='/newhealthrecord'
+                        element={<HealthRecord />}
+                    ></Route>
+                    <Route path='/breeds' element={<Breeds />}></Route>
+                    <Route
+                        path='/breeds/:breedname'
+                        element={<Breed />}
+                    ></Route>
+                    <Route path='*' element={<Home />}></Route>
+                </Routes>
+            </AppContext>
+        </BrowserRouter>
     );
 }
 
@@ -34,6 +48,6 @@ export default App;
        
         <Route path="/mycats/:id" element={<CatProfile />}></Route>
         <Route path="/breeds" element={<Breeds />}></Route>
-        <Route path="/breeds/:id" element={<BreedProfile />}></Route>
+        
         <Route path="/profile" element={<Profile />}></Route> 
 */
---
+++ client/src/pages/Breeds/Breeds.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import './Breeds.css';
+import ProfileLogo from '../../components/ProfileLogo/ProfileLogo';
+import PageTitle from '../../components/PageTitle/PageTitle';
+import { useNavigate,Link } from 'react-router-dom';
+export default function Breeds() {
+    const [catBreeds, setCatBreeds] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const fetchBreeds = async () => {
+            try {
+                const response = await fetch(
+                    'https://api.thecatapi.com/v1/breeds'
+                );
+                const catBreedsData = await response.json();
+                setCatBreeds(catBreedsData);
+                setIsLoading(!isLoading);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchBreeds();
+    }, []);
+
+    if (isLoading) return <h1>Loading...</h1>;
+
+    return (
+        <>
+            <Link to={'/mycats'}> button</Link>
+            <div className='home-text-container'>
+                <ProfileLogo />
+                <PageTitle title={'Breeds'} />
+            </div>{' '}
+            <div className='breed-container'>
+                {catBreeds.map((breed) => {
+                    return (
+                        <button
+                            key={breed.id}
+                            onClick={() => navigate(`/breeds/${breed.id}`)}
+                            className='breed-card'
+                        >
+                            <img
+                                src={`https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`}
+                                width='250'
+                                height='250'
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src =
+                                        'https://cdn2.thecatapi.com/images/dN6eoeLjY.jpg';
+                                }}
+                            />
+                            <h1>{breed.name}</h1>
+                        </button>
+                    );
+                })}
+            </div>
+        </>
+    );
+}
